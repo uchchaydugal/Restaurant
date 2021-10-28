a9f310a8663c5b1b1dec4ec12827fcc351c1f734
uchchaydugal <uchchaydugal@hotmail.com>
2021-10-28 16:11:37 +0530

    cleaned up Repository Interface

--- RestaurantOrderManagementProject/order_manager/src/main/java/com/restaurant/order_manager/repositories/IOrderRepository.java
+++ RestaurantOrderManagementProject/order_manager/src/main/java/com/restaurant/order_manager/repositories/IOrderRepository.java
@@ -1,16 +1,10 @@
 package com.restaurant.order_manager.repositories;
-import java.util.List;
 
 import com.restaurant.order_manager.models.Order;
-import com.restaurant.order_manager.models.State;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOrderRepository extends JpaRepository<Order, Long> {
     
-    //@Query("SELECT * FROM orders o WHERE o.state LIKE %?1%")
-    List<Order> findByState(State state);
 }
