0cc1f09d3a098e631b36e18fa4738de3e7b3009f
uchchaydugal <uchchaydugal@hotmail.com>
2021-10-28 19:35:12 +0530

    Test Cases Complete!!

--- RestaurantOrderManagementProject/order_manager/src/test/java/com/restaurant/order_manager/OrderManagerApplicationTests.java
+++ RestaurantOrderManagementProject/order_manager/src/test/java/com/restaurant/order_manager/OrderManagerApplicationTests.java
@@ -1,19 +1,26 @@
 package com.restaurant.order_manager;
 
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
 
 import com.restaurant.order_manager.models.Order;
 import com.restaurant.order_manager.models.State;
 import com.restaurant.order_manager.repositories.IOrderRepository;
-
-import org.junit.Test;
+import org.hamcrest.MatcherAssert;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class OrderManagerApplicationTests {
+@RunWith(SpringRunner.class)
+public class OrderManagerApplicationTests {
 
 	@Autowired
 	private IOrderRepository orderRepository;
@@ -26,7 +33,26 @@ class OrderManagerApplicationTests {
 		order.setTotalPrice(1200d);
 		order.state = State.open;
 
+		orderRepository.save(order);
+
 		assertNotNull(orderRepository.findById(order.getId()).get());
+	}
+
+	@Test
+	public void deleteOrder() {
+		Order orderToDelete = orderRepository.findById(11l).get();
+		orderRepository.deleteById(orderToDelete.getId());
+		assertNotEquals(true, orderRepository.findById(orderToDelete.getId()).isPresent());
+	}
+
+	@Test
+	public void editOrder() {
+		Order orderToEdit = orderRepository.findById(1l).get();
+		State state = orderToEdit.state;
+		orderToEdit.state = State.completed;
+		orderRepository.save(orderToEdit);
+
+		assertNotEquals(state, orderRepository.findById(orderToEdit.getId()).get().state);
 	}
 
 }
